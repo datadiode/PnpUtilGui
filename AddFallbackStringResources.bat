082a9fb1d9bc7e600e65b1a1d4ac37be52545635
Jochen Neubeck <jochen.neubeck.ext@siemens-energy.com>
2026-03-12 16:49:32 +0100

    Add fallback string resources to prepare for the case that PnPutil.exe.mui is missing / Don't requireAdministrator as it is not needed to just inspect what is there

---
+++ AddFallbackStringResources.bat
@@ -0,0 +1,14 @@
+REM Strings from recent PnPutil.exe.mui
+rcedit "PnpUtilGui\bin\Release\PnpUtilGui.exe" --set-resource-string 2201 "Published Name:     "
+rcedit "PnpUtilGui\bin\Release\PnpUtilGui.exe" --set-resource-string 2202 "Original Name:      "
+rcedit "PnpUtilGui\bin\Release\PnpUtilGui.exe" --set-resource-string 2204 "Provider Name:      "
+rcedit "PnpUtilGui\bin\Release\PnpUtilGui.exe" --set-resource-string 2205 "Class Name:         "
+rcedit "PnpUtilGui\bin\Release\PnpUtilGui.exe" --set-resource-string 2207 "Class GUID:         "
+rcedit "PnpUtilGui\bin\Release\PnpUtilGui.exe" --set-resource-string 2210 "Driver Version:     "
+rcedit "PnpUtilGui\bin\Release\PnpUtilGui.exe" --set-resource-string 2211 "Signer Name:        "
+REM Strings from legacy PnPutil.exe.mui
+rcedit "PnpUtilGui\bin\Release\PnpUtilGui.exe" --set-resource-string 4200 "Driver package provider :   "
+rcedit "PnpUtilGui\bin\Release\PnpUtilGui.exe" --set-resource-string 4300 "Class :                     "
+rcedit "PnpUtilGui\bin\Release\PnpUtilGui.exe" --set-resource-string 4400 "Driver date and version :   "
+rcedit "PnpUtilGui\bin\Release\PnpUtilGui.exe" --set-resource-string 4500 "Signer name :               "
+rcedit "PnpUtilGui\bin\Release\PnpUtilGui.exe" --set-resource-string 4600 "Published name :            "
